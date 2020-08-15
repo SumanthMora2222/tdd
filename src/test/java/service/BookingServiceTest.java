d7e00433e9213fe008a1e918287961bc19f682bc
Shruti Chakraborty <shrutic@thoughtworks.com>
2020-08-15 21:36:47 +0530

    Add test for, when not stored dont publish

--- src/test/java/service/BookingServiceTest.java
+++ src/test/java/service/BookingServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -41,5 +42,22 @@ public class BookingServiceTest {
     bookingService.book(orders);
 
     verify(bookingPublisher).publish(orders);
+  }
+
+  @Test
+  public void shouldNotPublishOrdersIfNotSaved() {
+    BookingPublisher bookingPublisher = mock(BookingPublisher.class);
+    BookingRepository bookingRepository = mock(BookingRepository.class);
+
+    final BookingService bookingService = new BookingService(bookingRepository, bookingPublisher);
+
+    final Order order = new Order("123456", "QWERASDF", "PENDING", "2019-08-21T11:05:03+00:00");
+    final List<Order> orders = Arrays.asList(order);
+
+    when(bookingRepository.add(orders)).thenReturn(false);
+
+    bookingService.book(orders);
+
+    verify(bookingPublisher,never()).publish(orders);
   }
 }
