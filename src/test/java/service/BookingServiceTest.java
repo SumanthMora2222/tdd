51e10b87f1b43fd88efee90efda01cdcc2c68357
Shruti Chakraborty <shrutic@thoughtworks.com>
2020-08-16 12:13:00 +0530

    Remove redundant test

--- src/test/java/service/BookingServiceTest.java
+++ src/test/java/service/BookingServiceTest.java
@@ -12,23 +12,9 @@ import model.Order;
 import org.junit.Test;
 
 public class BookingServiceTest {
-  @Test
-  public void shouldStoreOrder() {
-    BookingPublisher bookingPublisher = mock(BookingPublisher.class);
-    BookingRepository bookingRepository = mock(BookingRepository.class);
-
-    final BookingService bookingService = new BookingService(bookingRepository, bookingPublisher);
-
-
-    final Order order = new Order("123456", "QWERASDF", "PENDING", "2019-08-21T11:05:03+00:00");
-    final List<Order> orders = Arrays.asList(order);
-    bookingService.book(orders);
-
-    verify(bookingRepository).add(orders);
-  }
 
   @Test
-  public void shouldPublishOrdersIfSavedSuccesfully() {
+  public void shouldPublishOrdersIfSavedSuccessfully() {
     BookingPublisher bookingPublisher = mock(BookingPublisher.class);
     BookingRepository bookingRepository = mock(BookingRepository.class);
 
