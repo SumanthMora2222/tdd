f20ceddfbc06820d433e194af4ae490becebb752
Sumanth Kumar Mora <sumantkm@thoughtworks.com>
2021-02-27 02:07:46 +0530

    Add state bucket for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,10 @@ provider "google" {
   project = "otto-university-302315"
   region  = "europe-west3"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "sumanth-tfstate-bucket"
+    prefix = "tdd"
+  }
+}
